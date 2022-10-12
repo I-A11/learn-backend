f039244d99f316a90bb03acb67036905f30c96d7
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-10-12 15:02:20 +1100

    Create data folder to store user name.

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const http = require("http");
+const fs = require("fs");
 
-function handleRequest(request, response) {
-  if (request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end("<h1>" + new Date().toISOString() + "</h1>");
-  } else if (request.url === "/") {
-    response.end("<h1>Hello World!</h1>");
-  }
-}
+const path = require("path");
 
-const server = http.createServer(handleRequest);
+const express = require("express");
 
-server.listen(3000);
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/currenttime", function (req, res) {
+  res.send("<h1>" + new Date().toISOString() + "</h1>");
+});
+
+app.get("/", function (req, res) {
+  res.send(
+    `<form action="/store-user" method="POST"><label>Your Name </label><input type="text" name="username"> <button>Submit</button></form>`
+  );
+});
+
+app.post("/store-user", function (req, res) {
+  const userName = req.body.username;
+
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+
+  const existingUser = JSON.parse(fileData);
+
+  existingUser.push(userName);
+
+  fs.writeFileSync(filePath, JSON.stringify(existingUser));
+
+  res.send(`<h1>User name stored!</h1>`);
+});
+
+app.listen(3000);

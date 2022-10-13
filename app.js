e04a220cdee9de20369f246bd8cc3d02de985db4
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2022-10-13 12:02:52 +1100

    Create data file with json formate.

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/currenttime", function (req, res) {
 
 app.get("/", function (req, res) {
   res.send(
-    `<form action="/store-user" method="POST"><label>Your Name </label><input type="text" name="username"> <button>Submit</button></form>`
+    `<form action="/store-user" method="POST"><label>Your Name: </label><input type="text" name="username"> <button>Submit</button></form>`
   );
 });
 
@@ -25,13 +25,31 @@ app.post("/store-user", function (req, res) {
 
   const fileData = fs.readFileSync(filePath);
 
-  const existingUser = JSON.parse(fileData);
+  const existingUsers = JSON.parse(fileData);
 
-  existingUser.push(userName);
+  existingUsers.push(userName);
 
-  fs.writeFileSync(filePath, JSON.stringify(existingUser));
+  fs.writeFileSync(filePath, JSON.stringify(existingUsers));
 
   res.send(`<h1>User name stored!</h1>`);
+});
+
+app.get("/users", function (req, res) {
+  const filePath = path.join(__dirname, "data", "users.json");
+
+  const fileData = fs.readFileSync(filePath);
+
+  const existingUsers = JSON.parse(fileData);
+
+  let responseData = "<ul>";
+
+  for (const user of existingUsers) {
+    responseData += "<li>" + user + "</li>";
+  }
+
+  responseData += "</ul>";
+
+  res.send(responseData);
 });
 
 app.listen(3000);
